c164a0138e8c6f5921bb82ab6106d56a6af5609a
DorkYBru <76796721+DorkYBru@users.noreply.github.com>
2021-10-02 15:12:28 +0200

    ass

---
+++ scripts/download.js
@@ -0,0 +1,11 @@
+function download(url, filename) {
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = filename;
+        link.click();
+    })
+    .catch(console.error);
+  }
